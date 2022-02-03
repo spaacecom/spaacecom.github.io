27204a7cd6a92a8656b8101777b6624dce2b3072
rainbowfirestudios <rainbowfirestudios@gmail.com>
2022-02-03 15:10:45 -0500

    adsdasds

--- js/theme.js
+++ js/theme.js
@@ -10,7 +10,25 @@ function notyet() {
 }
 // Theme
 window.theme = {};
+var modal = document.getElementById("modalid");
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("fsimg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("captionfs");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closefs")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 // Theme Common Functions
 window.theme.fn = {
 
